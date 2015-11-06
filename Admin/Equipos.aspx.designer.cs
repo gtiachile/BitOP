655135cdbd06796048371196eee8f6161f004539
Andres Ulloa <andres.ulloa@gtia.cl>
2015-11-06 14:01:23 -0300

    Commit 06112015-1400

--- Admin/Equipos.aspx.designer.cs
+++ Admin/Equipos.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace BitOp.Admin {
         protected global::System.Web.UI.WebControls.SqlDataSource BopDBEquipos;
         
         /// <summary>
+        /// Control ImageButton1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton1;
+        
+        /// <summary>
         /// Control GridView1.
         /// </summary>
         /// <remarks>
